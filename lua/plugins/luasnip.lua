156af802b539d4cd197d0b17f39c68eb4032e9c1
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-01-21 16:47:37 -0500

    removed tab keymap from luasnip and added the prettier extra

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -6,5 +6,11 @@ return {
       region_check_events = "InsertEnter",
       delete_check_events = "TextChanged,InsertLeave",
     },
+    -- Vitally important as the randomn tab hiccups drive me fucking nuts
+    keys = {
+      { "<tab>", false, mode = { "i" } },
+      { "<tab>", false, mode = { "s" } },
+      { "<s-tab>", false, mode = { "i", "s" } },
+    },
   },
 }
