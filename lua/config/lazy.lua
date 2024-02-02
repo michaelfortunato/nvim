6c94576879e2b370bb06e2d9c6994c7959f175c8
Michael Fortunato <michael.n.fortunato@gmail.com>
2024-02-02 14:49:23 -0500

    removed annoyng lazyvim notifications on startup. And only having it check it once a day now

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, notify = false, frequency = 86400 }, -- automatically check for plugin updates, DO NOT notify. I hate that
   performance = {
     rtp = {
       -- disable some rtp plugins
